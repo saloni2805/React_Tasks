d6e18b704e79f686219a6b2e1a34c5d2814521e6
saloni2805 <salonispotdar@gmail.com>
2025-04-09 17:47:35 +0530

    `Update import statements and fix typos in NavBar component`

--- Notes_Project/src/Components/NavBar.jsx
+++ Notes_Project/src/Components/NavBar.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
-    return (
-        <div className='flex flex-row gap-4 place-content-evenly'>
-            <NavLink to='/'>
-                Home
-            </NavLink>
-            <NavLink to="/pastes">
-                Pastesuu
-            </NavLink>
-        </div>
-    )
+  return (
+    <div className="flex flex-row gap-4 place-content-evenly">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/pastes">Pastes</NavLink>
+    </div>
+  )
 }
 
 export default NavBar
